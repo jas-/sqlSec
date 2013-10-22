37d2b96c57b0aa85f30c8ee463850ac324e389d1
jas- <jason.gerfen@gmail.com>
2013-10-22 11:08:55 -0600

    Tue Oct 22 11:08:55 MDT 2013
    Install:
     Updated SQL files to reflect new variable names for replacements used
     by installation script.
     Fixed bug with default value for default backup folder preceeding forward
     slash.

--- templates/sqlSec-schema.sql
+++ templates/sqlSec-schema.sql
@@ -1,15 +1,15 @@
 -- Switch to newly created db context
-USE `[dbName]`;
+USE `{NAME}`;
 
 -- Assign grant priviledge for scheduled events account then drop it
-GRANT USAGE ON *.* TO `[dbUser]`@`[dbHost]`;
-DROP USER `[dbUser]`@`[dbHost]`;
+GRANT USAGE ON *.* TO `{SP}`@`{SERVER}`;
+DROP USER `{SP}`@`{SERVER}`;
 FLUSH PRIVILEGES;
 
 -- Create the scheduled events user and assign limited permissions
-CREATE USER `[dbUser]`@`[dbHost]` IDENTIFIED BY '[dbPass]';
-GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, CREATE TEMPORARY TABLES, LOCK TABLES, TRIGGER, EXECUTE, EVENT, CREATE VIEW, DROP ON `[dbName]`.* TO `[dbUser]`@`[dbHost]`;
-GRANT FILE ON *.* TO `[dbUser]`@`[dbHost]`;
+CREATE USER `{SP}`@`{SERVER}` IDENTIFIED BY '{SP_PW}';
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, CREATE TEMPORARY TABLES, LOCK TABLES, TRIGGER, EXECUTE, EVENT, CREATE VIEW, DROP ON `{NAME}`.* TO `{SP}`@`{SERVER}`;
+GRANT FILE ON *.* TO `{SP}`@`{SERVER}`;
 FLUSH PRIVILEGES;
 
 -- Drop & create new application settings table
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `sqlSec_settings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=0;
 
 -- Populate the settings table with defaults
-INSERT INTO `sqlSec_settings` (`version`, `epoch`, `keyID`) VALUES ('v0.1', UNIX_TIMESTAMP(NOW()), '[dbKey]');
+INSERT INTO `sqlSec_settings` (`version`, `epoch`, `keyID`) VALUES ('{VER}', UNIX_TIMESTAMP(NOW()), '{KEY}');
 
 -- Drop & create new table to manage table/fields which require symmetric encryption
 DROP TABLE IF EXISTS `sqlSec_map`;
@@ -36,12 +36,3 @@ CREATE TABLE IF NOT EXISTS `sqlSec_map` (
   UNIQUE KEY `field` (`field`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=0;
 
--- Testing table
-DROP TABLE IF EXISTS `myTest`;
-CREATE TABLE IF NOT EXISTS `myTest` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `myField` LONGTEXT NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=0;
-
-INSERT INTO `myTest` (`myField`) VALUES ("abd"),("def"),("hij");
